63318d02adc593450ca1b38456a62a75302e3561
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2025-12-05 19:47:16 +0200

    feat: add initial web routes for guest forms, Zoom OAuth, user registration, and quiz display

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GuestFormController;
 use App\Http\Controllers\QuizController as ControllersQuizController;
 
+// Guest Form Routes - Subdomain based (MUST be first!)
+// URL format: {subdomain}.form.iadcsuez.org
+Route::domain('{subdomain}.form.iadcsuez.org')->group(function () {
+    Route::get('/', [GuestFormController::class, 'show'])->name('form.show');
+    Route::post('/', [GuestFormController::class, 'submit'])->name('form.submit');
+});
+
+// Main domain routes
 Route::get('/', function () {
     return redirect()->route('login');
 })->name('index');
@@ -14,13 +22,4 @@ Route::get('/zoom/callback', [ZoomController::class, 'callback'])->name('zoom.ca
 Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register.post');
 
-
 Route::get('/quiz/{id}', [ControllersQuizController::class, 'showQuiz'])->name('quiz.show');
-
-// Guest Form Routes - Subdomain based
-// URL format: {subdomain}.form.iadcsuez.org
-Route::domain('{subdomain}.form.iadcsuez.org')->group(function () {
-    Route::get('/', [GuestFormController::class, 'show'])->name('form.show');
-    Route::post('/', [GuestFormController::class, 'submit'])->name('form.submit');
-});
-
