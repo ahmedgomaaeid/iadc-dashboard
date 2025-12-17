d95f9005699e3214d08da094d5e37e8378c3a16d
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2025-12-17 23:01:32 +0200

    fix routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,19 +12,17 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::domain(env('APP_URL'))->group(function () {
-                Route::middleware('web')
+            Route::middleware('web')
                 ->group(base_path('routes/user.php'));
 
-                Route::middleware('web')
+            Route::middleware('web')
                 ->group(base_path('routes/board.php'));
 
-                Route::middleware('web')
+            Route::middleware('web')
                 ->group(base_path('routes/highboard.php'));
 
-                Route::middleware('web')
+            Route::middleware('web')
                 ->group(base_path('routes/admin.php'));
-            });
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
