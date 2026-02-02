491ba1b7012d3f7cb1970a81fddc71543068a069
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2026-02-02 17:22:28 +0200

    add privacy to home page

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -216,6 +216,10 @@
                                             <a class="side-menu__item" data-bs-toggle="slide" href="#Contact"><span
                                                     class="side-menu__label">Contact</span></a>
                                         </li>
+                                        <li class="slide">
+                                            <a class="side-menu__item" href="#PrivacyPolicy"><span
+                                                    class="side-menu__label">Privacy Policy</span></a>
+                                        </li>
                                     </ul>
                                     <div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg"
                                             fill="#7b8191" width="24" height="24" viewBox="0 0 24 24">
