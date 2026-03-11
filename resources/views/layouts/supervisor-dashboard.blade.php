d1a323befeaeb0d8c9d7f7c941c20bd283d4932e
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2026-03-11 11:38:29 +0200

    hide mail

--- resources/views/layouts/supervisor-dashboard.blade.php
+++ resources/views/layouts/supervisor-dashboard.blade.php
@@ -191,11 +191,11 @@
                                                 class="side-menu__icon fe fe-video"></i><span
                                                 class="side-menu__label">Sessions</span></a>
                                     </li>
-                                    <li class="slide">
+                                    <!-- <li class="slide">
                                         <a class="side-menu__item has-link" data-bs-toggle="slide" href="{{route('supervisor.email.inbox')}}"><i
                                                 class="side-menu__icon fe fe-mail"></i><span
                                                 class="side-menu__label">Email</span></a>
-                                    </li>
+                                    </li> -->
 
                                     <li class="sub-category">
                                         <h3>Profile</h3>
