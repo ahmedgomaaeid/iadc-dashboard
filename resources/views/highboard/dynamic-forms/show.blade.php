76a9dff947bd95fd700364d74b2ae41407818053
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2026-04-12 17:01:39 +0200

    repair value

--- resources/views/highboard/dynamic-forms/show.blade.php
+++ resources/views/highboard/dynamic-forms/show.blade.php
@@ -23,6 +23,7 @@
     <div class="row">
 
         <div class="col-lg-12">
+            @php $orderedFields = $dynamicForm->getOrderedFields(); @endphp
             <div class="card">
                 <div class="card-header d-flex justify-content-between align-items-center">
                     <h3 class="card-title">Submissions ({{ $submissions->total() }})</h3>
